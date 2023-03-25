4ee53934fd65032105f386a38438b20c37497836
Mert Kavilcioğlu <mertkavilci@gmail.com>
2023-03-25 16:01:36 +0300

    Bubble Sort operation is written as a function.

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*void BubbleSort(int num[size]) 
+void BubbleSort(int num[] ,int n) 
 {
-	
+	int  j, k, temp;
+	for (k=1 ; k<=n-1 ; k++)
+		for (j=0 ; j<n-k ; j++)
+			if(num[j]>num[j+1])
+			{
+				temp = num[j];
+				num[j] = num[j+1];
+				num[j+1] = temp;
+			}
+		
 }
-*/
+
 
 int main()
 {
-	int n, i, num[n], j, k, temp;
+	int n, i, num[n];
 	printf("Enter the size of array:");
 	scanf("%d", &n);
 	printf("Enter %d integer values:", n);
@@ -22,7 +31,13 @@ int main()
 		printf("%4d", num[i]);
 		printf("\n");
 		
-
+	printf("After sorting :");
+	
+	BubbleSort(num, n);
+	
+	for(i=0 ; i<n ; i++)
+	printf("%4d", num[i]);
+	printf("\n");
 	system("PAUSE");
 	return 0;
 	
